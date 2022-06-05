11963c931051807ec48b4b2981594d5b6bcf3623
Jakub Królicki <101356174+Krolicki@users.noreply.github.com>
2022-06-05 20:10:24 +0200

    Przelicznie walut

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-const EURp = document.getElementById("EUR");
-const USDp = document.getElementById("USD");
+const converted = document.getElementById("converted");
+const calcButton = document.getElementById("calc");
+const opt = document.getElementById("opt");
+const amount = document.getElementById("amount");
+const calcDate = document.getElementById("calcDate");
 const eurLink = "http://api.nbp.pl/api/exchangerates/rates/A/EUR/?format=json";
 const usdLink = "http://api.nbp.pl/api/exchangerates/rates/A/USD/?format=json";
 
 
-function getRate(url, element){
-    let data = fetch(url)
+function getRate(url, element, am){
+    fetch(url)
     .then(resp => {
         return resp.json();
     })
     .then(resp => {
-        //EURp.innerHTML += resp.rates[0].mid;
-        return resp.rates[0].mid;
+        calcDate.innerHTML = "Na dzień: " + resp.rates[0].effectiveDate;
+        element.innerHTML = (resp.rates[0].mid * am).toFixed(2) + " PLN";
+        //return resp.rates[0].mid;
     })
     .catch(err => {
         console.log(err);
     });
 
-    let loadEur = async () => {
-        element.innerHTML += await data; 
-    }
-    loadEur();
+    // let loadEur = async () => {
+    //     element.innerHTML = (await data * am).toFixed(2) + " PLN"; 
+        
+    // }
+    // loadEur();
 
 }
 
-getRate(eurLink, EURp);
-getRate(usdLink, USDp);
+calcButton.addEventListener("click", function(){
+    let opti = opt.value;
+    let amo = amount.value;
+    switch(opti){
+        case 'EUR':
+            getRate(eurLink, converted, amo);
+            break;
+        case 'USD':
+            getRate(usdLink, converted, amo);
+            break;
+    }
+    
+
+})
