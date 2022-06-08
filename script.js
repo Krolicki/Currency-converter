4a426bc4a2f0fdb04ef04097382230b66caa2a7c
Jakub Królicki <101356174+Krolicki@users.noreply.github.com>
2022-06-08 22:54:01 +0200

    Przebudowa pobierania kursów

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const amount = document.getElementById("amount");
 const amountBot = document.getElementById("amountBot");
 const calcDate = document.getElementById("calcDate");
 const info = document.getElementById("info");
+const testButton = document.getElementById("testButton");
 const eurLink = "http://api.nbp.pl/api/exchangerates/rates/A/EUR/?format=json";
 const usdLink = "http://api.nbp.pl/api/exchangerates/rates/A/USD/?format=json";
 
+var test;
 
 function getRate(url, element, am, reverse){
     fetch(url)
@@ -25,12 +27,49 @@ function getRate(url, element, am, reverse){
             else
                 element.value = (resp.rates[0].mid * am).toFixed(2);
         }
-
     })
     .catch(err => {
         console.log(err);
     });
 }
+
+async function getRate2(url) {
+    // var response = await fetch(url)
+    // .then(resp => {
+    //     return resp.json();
+    // })
+    // .then(resp => {
+    //     return resp.rates[0].mid;
+    // });
+  
+    // return response;
+    const response = await fetch(url);
+    const data = await response.json();
+    const ret = data.rates[0].mid;
+    return ret;
+  }
+  
+  async function getRate3(url){
+      test = await getRate2(url);
+      //console.log(rate);
+     // test = rate.rates[0].mid;
+     // return rate.rates[0].mid;
+
+  }
+
+  function test2(){
+    getRate3(usdLink);
+  }
+  //const data2 = getRate3(usdLink);
+  getRate3(usdLink);
+  
+  test2();
+
+testButton.addEventListener("click",function(){
+    console.log(test);
+})
+
+
 
 amount.addEventListener("change", function(){
     let opti = opt.value;
