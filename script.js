1a5c691e82016454989a8856f8ac09e52ece3af5
Jakub Królicki <101356174+Krolicki@users.noreply.github.com>
2022-06-09 20:00:21 +0200

    Zapisywanie kursów, poprawa input

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ const amountBot = document.getElementById("amountBot");
 const calcDate = document.getElementById("calcDate");
 const info = document.getElementById("info");
 const testButton = document.getElementById("testButton");
+
 const eurLink = "http://api.nbp.pl/api/exchangerates/rates/A/EUR/?format=json";
 const usdLink = "http://api.nbp.pl/api/exchangerates/rates/A/USD/?format=json";
 
-var test;
+var eurRate;
+var usdRate;
 
 function getRate(url, element, am, reverse){
     fetch(url)
@@ -33,66 +35,50 @@ function getRate(url, element, am, reverse){
     });
 }
 
-async function getRate2(url) {
-    // var response = await fetch(url)
-    // .then(resp => {
-    //     return resp.json();
-    // })
-    // .then(resp => {
-    //     return resp.rates[0].mid;
-    // });
-  
-    // return response;
+async function getCurr(url) {
     const response = await fetch(url);
     const data = await response.json();
     const ret = data.rates[0].mid;
     return ret;
   }
   
-  async function getRate3(url){
-      test = await getRate2(url);
-      //console.log(rate);
-     // test = rate.rates[0].mid;
-     // return rate.rates[0].mid;
+  async function assign(){
+      usdRate = await getCurr(usdLink);
+      eurRate = await getCurr(eurLink);
 
   }
 
-  function test2(){
-    getRate3(usdLink);
+
+
+ function calcRate(rate, element, amount, rev){
+    if(element.nodeName == "INPUT"){
+        if(rev)
+            element.value = (amount / rate).toFixed(2);
+        else
+            element.value =  (amount * rate).toFixed(2);
   }
-  //const data2 = getRate3(usdLink);
-  getRate3(usdLink);
-  
-  test2();
-
-testButton.addEventListener("click",function(){
-    console.log(test);
-})
-
-
+}
 
 amount.addEventListener("change", function(){
     let opti = opt.value;
-    let amo = amount.value;
     switch(opti){
         case 'EUR':
-            getRate(eurLink, amountBot, amo, false);
+            calcRate(eurRate, amountBot, this.value);
             break;
         case 'USD':
-            getRate(usdLink, amountBot, amo, false);
+            calcRate(usdRate, amountBot, this.value);
             break;
     }
 })
 
 amountBot.addEventListener("change", function(){
     let opti = opt.value;
-    let amo = amountBot.value;
     switch(opti){
         case 'EUR':
-            getRate(eurLink, amount, amo, true);
+            calcRate(eurRate, amount, this.value, true);
             break;
         case 'USD':
-            getRate(usdLink, amount, amo, true);
+            calcRate(usdRate, amount, this.value, true);
             break;
     }
 })
@@ -102,17 +88,22 @@ opt.addEventListener("change", function(){
     let amo = amount.value;
     switch(opti){
         case 'EUR':
+            // if(optBot.value == 'EUR')
+            //     optBot.value = 'PLN';
             info.innerHTML = "1 Euro to w przeliczeniu";
             getRate(eurLink, converted, 1);
-            getRate(eurLink, amountBot, amo, false);
+            calcRate(eurRate, amountBot, amo);
             break;
         case 'USD':
+            // if(optBot.value == 'USD')
+            //     optBot.value = 'PLN';
             info.innerHTML = "1 Dolar amerykański to w przeliczeniu";
             getRate(usdLink, converted, 1);
-            getRate(usdLink, amountBot, amo, false);
-            break;
+            calcRate(usdRate, amountBot, amo);
     }
 
 })
 
-getRate(eurLink, converted, 1);
+getRate(eurLink, converted, 1, false);
+getRate(eurLink, amountBot, 1, false);
+assign();
